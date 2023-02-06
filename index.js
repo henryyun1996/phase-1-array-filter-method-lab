a90898d189ef6dd7f4a5039e552ec90162d60e3f
Henry <henrysungyun@gmail.com>
2023-02-05 23:18:20 -0800

    come back to this

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver){
+        return driver.toLowerCase().includes(name.toLowerCase());
+    });
+};
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(function(driver) {
+      return driver.toLowerCase().startsWith(name.toLowerCase());
+    });
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+      return driver.name.toLowerCase() === name.toLowerCase();
+    });
+  }
